91341e05ddcc4e78e64cfafcfb2c10d504aeb504
daniel-w-f <daniel_wilms@gmx.de>
2022-09-16 11:04:29 +0200

    Connect to Redis server

--- src/RedisJava.java
+++ src/RedisJava.java
@@ -1,9 +1,26 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import nl.melp.redis.Redis;
+
 /**
  * RedisJava
  */
 public class RedisJava {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("Hello World");
+
+        Redis r = new Redis(new Socket("127.0.0.1", 6379));
+        r.call("SET", "foo", "123");
+        r.call("INCRBY", "foo", "456");
+        Object o = r.call("GET", "foo");
+        System.out.println( o );
+        // System.out.println(r.call("GET", "foo")); // will print '579'
+        // how to convert char[] to proper string output to see
+        // "579"
+        // instead: [B@19e1023e
+
     }
 }
